2da708d5896663bdc47956a6818c52dbd37eda03
Devgadhvi <146376047+Devgadhvi@users.noreply.github.com>
2024-08-22 23:24:40 +0530

    include

--- index.php
+++ index.php
@@ -4,23 +4,14 @@
     <meta charset="UTF-8">
     <meta name="aurther" content="Dev-gadhvi">
     <meta name="keyword" content="Sahitya,sahitya,charan,gadhvi,gadhavi,bhajan,lyrics,">
-    <title>Document</title>
+    <title>Charani Sahitya</title>
     <head>
     <?php require_once 'includes/head.php'; ?>
 </head>
 </head>
 <body>
-    
-<header>
-    <h1>Welcome to My Website</h1>
-    <nav>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-    </nav>
-</header>
+<?php include 'includes/header.php';?> 
+
+<?php include 'includes/footer.php';?>
 </body>
 </html>

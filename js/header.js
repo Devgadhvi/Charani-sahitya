40b91e26ecde550012a7a4876136a50dded283b9
sanjana <sanjusharma8306@gmail.com>
2024-08-24 00:04:50 +0530

    added template

--- js/header.js
+++
@@ -1,7 +0,0 @@
-
-    // JavaScript to load the header.html content
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('').innerHTML = data;
-        });

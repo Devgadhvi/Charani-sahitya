1bc7ef15ff64117a3857a69b0d1a869a3c053d9e
sanjana <sanjusharma8306@gmail.com>
2024-08-24 00:22:18 +0530

    WIP: created common header footer

---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<div class="wrap-header">
+        <header class="header flat-header lh-header header-style1">
+            <div class="site-header-inner">
+                <div class="container">
+                    <div id="logo" class="logo">
+                        <a href="index.html"><img src="images/logo/02.png" alt="images"></a>
+                    </div>
+                    <div class="mobile-button"><span></span></div>
+                    <div class="header-menu">
+                        <nav id="main-nav" class="main-nav">
+                            <ul class="menu">
+                                <li><a href="#">Home</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item active"><a href="index.html">Home 1</a></li>
+                                        <li class="menu-item"><a href="home2.html">Home 2</a></li>
+                                        <li class="menu-item"><a href="home3.html">Home 3</a></li>
+                                        <li class="menu-item"><a href="home4.html">Home 4</a></li>
+                                        <li class="menu-item"><a href="home5.html">Home 5</a></li>
+                                    </ul><!-- sub-menu -->
+                                </li>
+                                <li><a href="course.html">Course</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item"><a href="course.html">Course</a></li>
+                                        <li class="menu-item"><a href="course-list.html">Course list</a></li>
+                                        <li class="menu-item"><a href="course-single.html">Course single</a></li>
+                                    </ul><!-- sub-menu -->
+                                </li>
+                                <li><a href="#">Explore</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item"><a href="about.html">About</a></li>
+                                    </ul><!-- sub-menu -->
+                                </li>
+                                <li><a href="#">Degrees</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item"><a href="blog.html">Blog</a></li>
+                                        <li class="menu-item"><a href="blog-single.html">Blog single</a></li>
+                                    </ul><!-- sub-menu -->
+                                </li>
+                                <li class="nav-sing">
+                                    <a class="sing-in" href="#">Sing In</a>
+                                    <a class="sing-up" href="#">Sing Up</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div> 
+                </div>
+            </div>
+        </header><!-- header -->
+    </div>

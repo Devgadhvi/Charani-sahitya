cca3a60183a2c85ffc1b182ee1639bdc24183938
sanjana <sanjusharma8306@gmail.com>
2024-08-25 03:05:05 +0530

    active menu in header  css fixes

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
 <div class="wrap-header">
     <header class="header flat-header lh-header header-style1">
         <div class="site-header-inner">
@@ -13,29 +16,13 @@
                                 <div class="input-group input-group-sm mb-3p">
                                     <input type="text" class="form-control">
                                     <div class="input-group-append">
-                                        <button class="btn btn-secondary " type="button">
+                                        <button class="btn" type="button">
                                             <i class="fa fa-search"></i>
                                     </div>
                                 </div>
                             </li>
-                            <li><a href="#">Home</a>
-                                <ul class="sub-menu">
-                                    <li class="menu-item active"><a href="index.html">Home</a></li>
-                                    <li class="menu-item"><a href="home2.html">Home 2</a></li>
-                                </ul><!-- sub-menu -->
-                            </li>
-                            <li><a href="course.html">Course</a>
-                                <ul class="sub-menu">
-                                    <li class="menu-item"><a href="course.html">Course</a></li>
-
-                                </ul><!-- sub-menu -->
-                            </li>
-                            <li><a href="#">Explore</a>
-                                <ul class="sub-menu">
-                                    <li class="menu-item"><a href="about.html">About</a></li>
-                                </ul><!-- sub-menu -->
-                            </li>
-
+                            <li><a href="index.php" class="menu-item <?php echo ($current_page == 'index.php') ? 'active' : ''; ?>">Home</a></li>
+                            <li><a href="poets.php" class="menu-item <?php echo ($current_page == 'poets.php') ? 'active' : ''; ?>">Poets</a>
                         </ul>
                     </nav>
                 </div>

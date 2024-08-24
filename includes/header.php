34b5ad5f6b9d6698b98ef4aae7be62a7483a0c58
Devgadhvi <146376047+Devgadhvi@users.noreply.github.com>
2024-08-24 16:28:34 +0530

    added search bar

--- includes/header.php
+++ includes/header.php
@@ -1,36 +1,45 @@
 <div class="wrap-header">
-        <header class="header flat-header lh-header header-style1">
-            <div class="site-header-inner">
-                <div class="container">
-                    <div id="logo" class="logo">
-                        <a href="index.html" class="text-xl-start" > Charani Sahitya</a>
-                    </div>
-                    <div class="mobile-button"><span></span></div>
-                    <div class="header-menu">
-                        <nav id="main-nav" class="main-nav">
-                            <ul class="menu">
-                                <li><a href="#">Home</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item active"><a href="index.html">Home</a></li>
-                                        <li class="menu-item"><a href="home2.html">Home 2</a></li>
-                                    </ul><!-- sub-menu -->
-                                </li>
-                                <li><a href="course.html">Course</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item"><a href="course.html">Course</a></li>
-                                       
-                                    </ul><!-- sub-menu -->
-                                </li>
-                                <li><a href="#">Explore</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item"><a href="about.html">About</a></li>
-                                    </ul><!-- sub-menu -->
-                                </li>
+    <header class="header flat-header lh-header header-style1">
+        <div class="site-header-inner">
+            <div class="container">
+                <div id="logo" class="logo">
+                    <a href="index.html" class="text-xl-start"> Charani Sahitya</a>
+                </div>
+                <div class="mobile-button"><span></span></div>
+                <div class="header-menu">
+                    <nav id="main-nav" class="main-nav">
+                        <ul class="menu">
+                            <li>
+                                <div class="input-group input-group-sm mb-3p">
+                                    <input type="text" class="form-control">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-secondary " type="button">
+                                            <i class="fa fa-search"></i>
+                                    </div>
+                                </div>
+                            </li>
+                            <li><a href="#">Home</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item active"><a href="index.html">Home</a></li>
+                                    <li class="menu-item"><a href="home2.html">Home 2</a></li>
+                                </ul><!-- sub-menu -->
+                            </li>
+                            <li><a href="course.html">Course</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item"><a href="course.html">Course</a></li>
 
-                            </ul>
-                        </nav>
-                    </div> 
+                                </ul><!-- sub-menu -->
+                            </li>
+                            <li><a href="#">Explore</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item"><a href="about.html">About</a></li>
+                                </ul><!-- sub-menu -->
+                            </li>
+
+                        </ul>
+                    </nav>
                 </div>
             </div>
-        </header><!-- header -->
-    </div>
+        </div>
+    </header><!-- header -->
+</div>
